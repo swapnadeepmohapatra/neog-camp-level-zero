14640b7cb0acf13b71665f21f387bf46a6bbc5b6
Swapnadeep Mohapatra <41564532+swapnadeepmohapatra@users.noreply.github.com>
2022-08-19 23:32:08 +0530

    [Fixed] Error when no input (fun with triangles)

--- Mark12/src/angle-triangle.js
+++ Mark12/src/angle-triangle.js
@@ -7,16 +7,21 @@ function sumOfAngles(angle1, angle2, angle3) {
 }
 
 function isTriangle() {
-  const sum = sumOfAngles(
-    Number(inputs[0].value),
-    Number(inputs[1].value),
-    Number(inputs[2].value)
-  );
+  if (inputs[0].value && inputs[1].value && inputs[2].value) {
+    const sum = sumOfAngles(
+      Number(inputs[0].value),
+      Number(inputs[1].value),
+      Number(inputs[2].value)
+    );
 
-  outputEl.innerText =
-    sum === 180
-      ? "Yay, the angles form a triangle!"
-      : "Oh No! The angle doesn't form a triangle";
+    outputEl.innerText =
+      sum === 180
+        ? "Yay, the angles form a triangle!"
+        : "Oh No! The angle doesn't form a triangle";
+  } else {
+    alert("Please enter all the angles");
+    outputEl.innerText = "";
+  }
 }
 
 isTriangleBtn.addEventListener("click", isTriangle);
--- Mark12/src/calc-hypo.js
+++ Mark12/src/calc-hypo.js
@@ -8,12 +8,17 @@ function calculateSumOfSquare(a, b) {
 }
 
 function calculateHypotenuse() {
-  const sumOfSquares = calculateSumOfSquare(
-    Number(sides[0].value),
-    Number(sides[1].value)
-  );
-  const lengthOfHypotenuse = Math.round(Math.sqrt(sumOfSquares) * 100) / 100;
-  outputEl.innerText = "The length of hypotenuse is " + lengthOfHypotenuse;
+  if (!sides[0].value || !sides[1].value) {
+    alert("Please enter all the two sides");
+    outputEl.innerText = "";
+  } else {
+    const sumOfSquares = calculateSumOfSquare(
+      Number(sides[0].value),
+      Number(sides[1].value)
+    );
+    const lengthOfHypotenuse = Math.round(Math.sqrt(sumOfSquares) * 100) / 100;
+    outputEl.innerText = "The length of hypotenuse is " + lengthOfHypotenuse;
+  }
 }
 
 calculateBtn.addEventListener("click", calculateHypotenuse);
