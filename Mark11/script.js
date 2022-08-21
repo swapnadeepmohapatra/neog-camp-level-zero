0839f6522e3f35931ab03dc549f5b2b37ee2daec
Swapnadeep Mohapatra <41564532+swapnadeepmohapatra@users.noreply.github.com>
2022-08-21 21:01:47 +0530

    [Updated] Comments

--- Mark11/script.js
+++ Mark11/script.js
@@ -1,3 +1,7 @@
+/**
+ * Lucky Birthday App
+ * @param {HTMLElement} el HTMlelement where the app is rendered
+ */
 function LuckyBirthday(el) {
   const inputDob = document.createElement("input");
   const inputLucky = document.createElement("input");
@@ -8,6 +12,14 @@ function LuckyBirthday(el) {
 
   addEventListeners(button);
 
+  /**
+   * Creates the elements for the app and appends them to the dom
+   * @param {HTMLElement} el HTMlelement where the app is rendered
+   * @param {HTMLElement} inputDob HTMlelement for the input dob
+   * @param {HTMLElement} inputLucky HTMlelement for the input lucky
+   * @param {HTMLElement} button HTMlelement for the button
+   * @param {HTMLElement} output HTMlelement for the output
+   */
   function createElements(el, inputDob, inputLucky, button, output) {
     const fragment = document.createDocumentFragment();
 
@@ -32,6 +44,13 @@ function LuckyBirthday(el) {
     el.appendChild(fragment);
   }
 
+  /**
+   * Creates element with the given attributes and appends it to the dom
+   * @param {String} elem Element type
+   * @param {String} id Element id
+   * @param {String} innertext Inner text
+   * @param {HTMLElement} parent Parent element
+   */
   function createHtmlElem(elem, id, innertext, parent) {
     const element = document.createElement(elem);
     element.id = id;
@@ -39,6 +58,10 @@ function LuckyBirthday(el) {
     parent.appendChild(element);
   }
 
+  /**
+   * Adds event listeners to the button
+   * @param {*} button Event listener for the button
+   */
   function addEventListeners(button) {
     button.addEventListener("click", function (e) {
       e.preventDefault();
@@ -53,6 +76,11 @@ function LuckyBirthday(el) {
     });
   }
 
+  /**
+   * Calculates the sum of digits of the date
+   * @param {String} date Date of birth
+   * @returns sum of the digits of the date
+   */
   const calculateSum = (date) => {
     return date
       .replaceAll("-", "")
@@ -60,12 +88,21 @@ function LuckyBirthday(el) {
       .reduce((sum, digit) => sum + Number(digit), 0);
   };
 
+  /**
+   * Checks luckiness of the number with the sum of the date and displays the message
+   * @param {Number} sumOfDate Sum of the digits of the date
+   * @param {Number} numberToCheck Number to check with the sum of the date
+   */
   const checkIsNumberLucky = (sumOfDate, numberToCheck) => {
     sumOfDate % numberToCheck === 0
       ? showMessage(`${numberToCheck} is a lucky number!! 🥳 🥳 🥳 `)
       : showMessage(`${numberToCheck} is not that lucky 😕`);
   };
 
+  /**
+   * Display message in the output element
+   * @param {String} message Message to display
+   */
   const showMessage = (message) => {
     output.innerText = message;
   };
